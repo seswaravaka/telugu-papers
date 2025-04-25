0291d28e848803de75b640eaea4978d066c8264c
Sasi <seswaravaka@users.noreply.github.com>
2025-04-25 09:50:27 -0500

    Create list-of-houses.js

---
+++ list-of-houses.js
@@ -0,0 +1,7 @@
+const icons = [
+            { addr: 'Longgrove', url: 'https://ssl.cdn-redfin.com/photo/68/bigphoto/113/12310113_', icon: '🏠' },
+            { addr: 'Longgrove', url: "https://ssl.cdn-redfin.com/photo/68/bigphoto/301/12339301_", icon: '🏠' },
+            { addr: 'Longgrove', url: "https://ssl.cdn-redfin.com/photo/68/bigphoto/683/12323683_", icon: '🏠' },
+            { addr: '3937 Lakeview Ct, Long Grove, IL 60047', url: "https://ssl.cdn-redfin.com/photo/68/bigphoto/463/12341463_", icon: '🏠' },
+            { addr: 'Lincolnshire, 1.5M', url: 'https://ssl.cdn-redfin.com/photo/68/bigphoto/302/12347302_', icon: '🏠' }
+        ];
